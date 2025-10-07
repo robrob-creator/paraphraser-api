84d8700da5f21a7bd17bde038d78607d0246e51d
robert-moonshot <robertcumahig@gmail.com>
2025-10-07 10:44:00 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ParaphraserModule } from './paraphraser/paraphraser.module';
 
 @Module({
-  imports: [ParaphraserModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 86400000, // 24 hours in milliseconds
+        limit: 5, // 5 requests per day
+      },
+    ]),
+    ParaphraserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
